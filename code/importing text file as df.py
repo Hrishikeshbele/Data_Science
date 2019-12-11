b3a9b1d0cce83b00a4fd668b976bd9836c57b9d9
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-12-11 17:48:30 +0530

    Update importing text file as df.py

--- code/importing text file as df.py
+++ code/importing text file as df.py
@@ -5,10 +5,8 @@ df = pd.DataFrame(columns=['A','B','C','D'])
 i=0
 with open(r'CANCER.txt', 'r') as f:
     for line in f:
-        #splitting line w.r.t ',' and storing in x
-        x=line.split(',')
-        #striping '\n' from last elm of x
-        x[-1] = x[-1].strip('\n')
-        #adding new row at ith index 
-        df.loc[i,:] =x
+        #x=line.split(',') #other approach
+        #x[-1] = x[-1].strip('\n') # striping '\n' from last elm of x
+        #striping '\n' from last of line,splitting line w.r.t ','and adding new row at ith index 
+        df.loc[i,:] =line.rstrip('\n').split(',')
         i+=1
