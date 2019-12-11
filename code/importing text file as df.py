a28f7390be8694cda2809cb5c3b33296ed72f225
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-12-11 16:56:53 +0530

    Update importing text file as df.py

--- code/importing text file as df.py
+++ code/importing text file as df.py
@@ -1,9 +1,14 @@
 import os
+import pandas as pd
 os.chdir(r"D:\DS_challenges\CS4003 DA Projects\CS4003 Projects\Data Set")
+df = pd.DataFrame(columns=['A','B','C','D'])
 i=0
 with open(r'CANCER.txt', 'r') as f:
     for line in f:
+        #splitting line w.r.t ',' and storing in x
         x=line.split(',')
+        #striping '\n' from last elm of x
         x[-1] = x[-1].strip('\n')
+        #adding new row at ith index 
         df.loc[i,:] =x
         i+=1
