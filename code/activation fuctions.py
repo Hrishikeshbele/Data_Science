98ece11a51c4c3980daadc65a9c2df152b1a3360
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-03-01 16:46:18 +0530

    Update activation fuctions.py

--- code/activation fuctions.py
+++ code/activation fuctions.py
@@ -6,3 +6,9 @@ def sigmoid(x):
 def softmax(self, X):
     exps = np.exp(X)
     return exps / np.sum(exps, axis=1).reshape(-1,1)
+
+# 3.tanh function
+def tanh(x):
+    t=(np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+    dt=1-t**2
+    return t,dt # t is fucntion value and dt is derivative value
