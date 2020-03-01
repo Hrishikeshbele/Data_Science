6ae0b8865c2e6236f937347103ac5e16d3207ac8
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2020-03-01 16:41:27 +0530

    Update activation fuctions.py

--- code/activation fuctions.py
+++ code/activation fuctions.py
@@ -1,3 +1,8 @@
 # 1.sigmoid function:
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+# 2.softmax function
+def softmax(self, X):
+    exps = np.exp(X)
+    return exps / np.sum(exps, axis=1).reshape(-1,1)
